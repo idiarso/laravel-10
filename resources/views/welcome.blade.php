52d2e64f9661fb57f28b0febb4a15e63b106f638
Mas idi <idiarsosimbang@gmail.com>
2024-04-12 20:49:17 +0700

    com 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Online Store') @section('content')
+<div class="text-center"> <h2>Selamat Datang Di Toko Online Saya</h2>
+</div>
+@endsection
+
